98d8520bb1157041bbec8cc9f07adfa78ff61d43
최진호 <jhchoi@idevel.co..kr>
2021-11-04 15:32:17 +0900

    board3

--- page/board/thread_ok.php
+++ page/board/thread_ok.php
@@ -8,17 +8,17 @@ include $_SERVER['DOCUMENT_ROOT']."/db.php";
 // $data = mysql_fetch_array($result);
 // $top = $data[0] + 1;
 // 답글다는거
-$name = $_GET['name'];
-if(!$top){
-    $sql2 = mq("select max(top) from board where name $name");
-    $data = $sql2->fetch_array();
-    $top = $data[0] + 1;
-    }
+// $name = $_GET['name'];
+
+//     $sql2 = mq("select max(top) from board where name $name");
+//     $data = $sql2->fetch_array();
+//     $top = $data[0] + 1;
+    
 $date = date('Y-m-d');
 
 $sql = mq("insert into board(name,title,content,date,top,level) 
         values('".$_POST['name']."','".$_POST['title']."','".$_POST['content']."'
-        ,'".$date."','".$top."',1)");
+        ,'".$date."','".$_POST['top']."',1)");
 
 
 // $date = date('Y-m-d');
